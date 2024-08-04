b5d57de0f3997abd237a2c8c82fe22e662889d3c
Fawzia Rahimi <fawzia.rahimi13780@gmail.com>
2024-08-04 16:21:23 +0430

    Adding the book file and Wordcount file

---
+++ WordCount.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class WordCount {
+    public static void main (String[] arg) throws Exception {
+    File file = new File("A Tale of Two Cities");
+    Scanner scanner = new Scanner(file);
+
+    }
+}
